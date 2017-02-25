975df3179309fdf00c9db14c03f2735d21d9cb87
Korede Aderele <kaderele@gmail.com>
2017-02-25 14:22:46 -0500

    Add Flask boilerplate code

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+app = Flask(__name__, template_folder="views")
+
+app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
+
+@app.route("/")
+def index():
+    return render_template("index.pug", title="FAQ Chat")
